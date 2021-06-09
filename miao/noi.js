5d503a4c01df500be214f90742b140d1dfbe3cfc
kk <1940192924qq.com>
2021-06-09 16:21:38 +0800

    kk

--- miao/noi.js
+++ miao/noi.js
@@ -127,3 +127,195 @@ if (x > y) {
 } else {
   console.log('<');
 }
+
+var a = 54
+if (a >= 10 && a <= 99) {
+  console.log('1')
+} else {
+  console.log('0');
+}
+
+var a = 11, b = 19
+
+if (a > 10) {
+  console.log('1');
+} else if (b > 20) {
+  console.log('1');
+} else {
+  console.log('0')
+}
+var a = 9
+if (a % 3 === 0) {
+  if (a % 5 == 0) {
+    console.log('YES');
+  }
+    else {
+      console.log('NO');
+    }
+} else {
+  console.log('NO');
+}
+
+var a = 35
+if (a % 3 === 0) {
+  if (a % 5 == 0) {
+    if (a % 7 == 0) {
+      console.log(3, 5, 7);
+    }
+    else {
+      console.log(3, 5);
+    }
+  }
+  else if (a % 7 == 0) {
+    console.log(3, 7);
+  }
+  else {
+    console.log(3);
+  }  
+} else if (a % 5 == 0) {
+  if (a % 7 == 0) {
+    console.log(5, 7);
+  }
+  else {
+    console.log(5);
+  }
+} else if (a % 7 == 0) {
+  console.log(7);
+} else {
+  console.log('n');
+}
+
+var a = 50, b = 80
+if (a < 60) {
+  console.log(1);
+} else if (b < 60) {
+  console.log(1);
+} else {
+  console.log(0);
+}
+var a = 1
+if (a != 1 && a != 3 && a != 5) {
+  console.log('YES');
+} else {
+  console.log("NO");
+}
+
+var a = 120
+if (((a / 3) + 50) > (a/1.2)) {
+  console.log('Walk');
+} else if (((a / 3) + 50) < (a / 1.2)) {
+  console.log('Bike');
+} else {
+  console.log('All');
+}
+
+var x = 1.0
+
+if (x>=0&&x<5) {
+  console.log((-x+ 2.5).toFixed(3));
+} else if (x >= 5 && x < 10) {
+  console.log((2-1.5*(x-3)*(x-3)).toFixed(3));
+} else if (x >= 10 && x < 20) {
+  console.log((x / 2 -1.5).toFixed(3));
+} else {
+  console.log('你有问题');
+}
+
+var a = 1200, b = 'y'
+
+if (a > 1000) {
+   c = 8 + (Math.ceil((a -1000)/500)*4)
+} else {
+  c =8 
+}
+
+if (b == 'y') {
+  console.log(c+5);
+} else {
+  console.log(c);
+}
+
+var a = 10, b = 20, c = 56
+if (a > b) {
+  if (a > c) {
+    console.log(a);
+  }
+  else {
+    console.log(b);
+  }
+} else if(b > c) {
+  console.log(b);
+} else {
+  console.log(c);
+}
+
+var a = 3, b = 4, c = 5
+
+if (a * a + b * b == c * c) {
+  console.log('yes');
+} else {
+  console.log('no');
+}
+
+var a = 2012
+
+if (a % 4 == 0 || a % 100 == 0 && a % 400 == 0) {
+  console.log('Y');
+} else {
+  console.log('N');
+}
+
+var x1 = 1, y1 = -1, x2 = 1, y2 = 1, x3 = -1, y3 = -1, x4 = -1, y4 = -1
+
+a = Math.max(x1, x2, x3, x4)
+b = Math.max(y1, y2, y3, y4)
+e = Math.min(x1, x2, x3, x4)
+f = Math.min(y1, y2, y3, y4)
+var c = -1, d = -1
+if (c > 0) {
+  if (c > a || Math.abs(d) > b) {
+    console.log('no');
+  } else {
+    console.log('yes');
+  }
+} else if (c < 0) {
+  if (c < e || Math.abs(d) > Math.abs(f)) {
+    console.log('no');
+  } else {
+    console.log('yes');
+  }
+}
+
+var a = 1, b = 2 , c ='+'
+
+if (c == "+") {
+  console.log(a+b);
+} else if (c == "-") {
+  console.log(a-b);
+} else if (c == '*') {
+  console.log(a*b);
+} else if (c == '/') {
+  if (b == 0) {
+    console.log('Divided by zero!');
+  } else {
+    console.log(a/b);
+  }
+} else {
+  console.log('Invalid operator!')
+}
+var a = 1, b = 0, c = 1
+if (b * b == 4 * a * c) {
+  console.log('x1=x2='+((-b + Math.sqrt(b*b-4*a*c))/(2*a)));
+} else if (b * b > 4 * a * c) {
+  console.log("x1=" + (-b + Math.sqrt(b*b-4*a*c))/(2*a)+";"+"x2="+(-b - Math.sqrt(b*b-4*a*c))/(2*a));
+} else {
+  console.log("x1=" + ((-b / (2*a))).toFixed(5) + "+" + ((Math.sqrt(4*a*c-b*b) / (2*a))).toFixed(5)+"i" + ";" +'x2=' + ((-b / (2*a))).toFixed(5) + "-" +((Math.sqrt(4*a*c-b*b) / (2*a))).toFixed(5)+"i" );
+
+}
+
+
+
+var a = 10, b = 4, c = 9
+
+console.log(a - (Math.ceil(c / b)));
+
