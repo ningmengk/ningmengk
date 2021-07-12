0ceb5c4ce9de1b9977fff2c300675602ef213cdb
kk <1940192924qq.com>
2021-07-12 17:51:49 +0800

    kk

--- miao/lodash/ningmengk-lodash.js
+++ miao/lodash/ningmengk-lodash.js
@@ -1,8 +1,8 @@
 var ningmengk = function () {
   function chunk(arr, n) {
-    let a = [];
-    let b = []
-    for (let i = 0; i < arr.length; i++){
+    var a = [];
+    var b = []
+    for (var i = 0; i < arr.length; i++){
       a.push(arr[i])
       if (i == n) {
         b.push(a)
@@ -15,8 +15,8 @@ var ningmengk = function () {
     return b
   }
   function compact(arr) {
-    let a = []
-    for (let x of arr) {
+    var a = []
+    for (var x of arr) {
       if (x !== false && x !== null && x !== undefined && x != 0 && x !== '' && x !== NaN) {
         a.push(x)
       }
@@ -24,10 +24,43 @@ var ningmengk = function () {
     return a
   }
   function difference(arr, [value]) {
-    let a = new Set(arr)
-    for (let x of [value]) {
-      if(a.has(x)) a.delete(x)
+    var a = new Set(arr)
+    for (var x of [value]) {
+      if(a.has(x)) a.devare(x)
     }
+    return a
   }
-  return a
-}
+  function drop(arr, n=1) {
+    var a = []
+    if (n > arr.length) return a
+    for (var i = n; i < arr.length; i++){
+      a.push(arr[i])
+    }
+    return a
+  }
+  function dropright(arr, n=1) {
+    var a = []
+    var b= arr.length;
+    if (n > b) return a
+    for (var i = 0; i < b - n ; i++){
+      a.push(arr[i])
+    }
+    return a
+  }
+  function sum(arr) {
+    var sum = 0
+    for (var x of arr) {
+      sum += x
+    }
+    return sum
+  }
+  return {
+    chunk,
+    compact,
+    difference,
+    drop,
+    dropright,
+    sum,
+
+  }
+}()
